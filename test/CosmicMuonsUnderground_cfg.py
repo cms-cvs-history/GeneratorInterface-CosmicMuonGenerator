6d63a4a781bf4475e6c1260a89b4fb80aaf3baef
Lars Sonnenschein <sha1-a57afd2f0b08e67a847818a4abaa2c774bef8f5c@cern.ch>
2009-10-14 14:09:17 +0000

    added some functionality to SingleParticleEvent class for backward eloss computation; Fixed waterEquivalents bug for vertical muons; Added e_loss root script for convenient computation of backward energy loss via Newton minimisation

--- test/CosmicMuonsUnderground_cfg.py
+++ test/CosmicMuonsUnderground_cfg.py
@@ -8,8 +8,8 @@ process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(500)
-#    input = cms.untracked.int32(10)
+#    input = cms.untracked.int32(500)
+    input = cms.untracked.int32(20)
 #    input = cms.untracked.int32(100000)
 )
 process.CMSCGEN_out = cms.OutputModule("PoolOutputModule",
@@ -26,6 +26,7 @@ process.CosMuoGenSource.MinP = 3.
 process.CosMuoGenSource.MaxTheta = 89.
 
 # Plug z-position [mm] (default=-14000. = on Shaft) 
+#process.CosMuoGenSource.PlugVz = 5000.;
 #process.CosMuoGenSource.PlugVz = -33000.;
 
 # z-position of centre of target cylinder [mm] (default=0.)
@@ -34,8 +35,10 @@ process.CosMuoGenSource.MaxTheta = 89.
 #process.CosMuoGenSource.TrackerOnly = True
 
 #Read in Multi muon events or generate single muon events (MultiMuon=false = default)
-####process.CosMuoGenSource.MultiMuon = True
-####process.CosMuoGenSource.MultiMuonNmin = 2
+process.CosMuoGenSource.MultiMuon = True
+#process.CosMuoGenSource.MultiMuonNmin = 1
+#process.CosMuoGenSource.MultiMuonNmin = 2
+process.CosMuoGenSource.MultiMuonNmin = 3
 #process.CosMuoGenSource.MultiMuonFileName = "MultiEventsIn.root"
 #process.CosMuoGenSource.MultiMuonFileName = "test_25gev.root"
 #process.CosMuoGenSource.MultiMuonFileName = "test_150gev.root"
@@ -43,6 +46,10 @@ process.CosMuoGenSource.MaxTheta = 89.
 #process.CosMuoGenSource.MultiMuonFileName = "test_3000gev.root"
 ##process.CosMuoGenSource.MultiMuonFileName = "CORSIKA6900_800_3000GeV_1k.root"
 ####process.CosMuoGenSource.MultiMuonFileName = "CORSIKA6900_800_3000GeV_10k.root"
-#process.CosMuoGenSource.MultiMuonFileName = "CORSIKA6900_3_10TeV_100k.root"
-###process.CosMuoGenSource.MultiMuonFileFirstEvent = 1;
+process.CosMuoGenSource.MultiMuonFileName = "CORSIKA6900_3_10TeV_100k.root"
+#process.CosMuoGenSource.MultiMuonFileName = "CORSIKA6900_800_3000GeV_100k.root"
+###process.CosMuoGenSource.MultiMuonFileFirstEvent = 1
  
+
+
+#process.CosMuoGenSource.Verbosity = True
